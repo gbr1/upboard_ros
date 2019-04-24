d5e6595ec40542e0879aa497dbbb3f122c6d11c5
gbr1 <giovannididio.bruno@gmail.com>
2019-04-25 00:55:02 +0200

    added gpio digital input

--- src/gpio_node.cpp
+++ src/gpio_node.cpp
@@ -31,6 +31,9 @@
 
 #include <vector>
 
+float frequency;
+std::string frame_id;
+
 class pGpio : public mraa::Gpio{
     private:
         int _pin;
@@ -44,6 +47,8 @@ class pGpio : public mraa::Gpio{
 };
 
 std::vector<pGpio*> listgpio_output;
+std::vector<pGpio*> listgpio_input;
+
 
 
 void setGpio(uint8_t pin, uint8_t value){
@@ -66,6 +71,41 @@ void setGpio(uint8_t pin, uint8_t value){
     }
 }
 
+uint8_t getGpio(uint8_t pin){
+    uint8_t value=0;
+    int k=0;
+    bool found=false;
+    while((!found) && (k<listgpio_input.size())){
+        if (listgpio_input[k]->getPin()==pin){
+            found=true;
+            try{
+                value = listgpio_input[k]->read();
+            }
+            catch(std::exception& e){
+                ROS_ERROR("%s",e.what());
+                value= 254;
+            } 
+        }
+        k++;
+    }
+    if (!found){
+        ROS_WARN("GPIO %d is not defined. Check your configs",pin);
+        value=255;
+    }
+    return value;
+}
+
+bool checkDOParam(uint8_t pin){
+    int k=0;
+    while(k<listgpio_output.size()){
+        if (listgpio_output[k]->getPin()==pin){
+            return true;
+        }
+        k++;
+    }
+    return false;
+}
+
 
 void gpioCallback(const upboard_ros::ListGpio & msg){
     for (int i=0; i<msg.gpio.size(); i++){
@@ -79,6 +119,23 @@ int main(int argc, char **argv){
     
     /* rostopic pub /upboard/gpio/write upboard_ros/ListGpio "{header: auto, gpio:[{pin: 22, value: 0}]}" --once */
     
+    //rate parameter
+    if (nh.getParam("upboard_ros/frequency", frequency)){
+        ROS_INFO("Node rate is %f Hz",frequency);
+    }else{
+        frequency=100.0;
+        ROS_INFO("[Default] Node rate is %f Hz",frequency);
+    }
+
+    //frame id parameter
+    if (nh.getParam("upboard_ros/frame_id", frame_id)){
+        ROS_INFO("Frame Id is %s",frame_id.c_str());
+    }else{
+        frame_id="base_link";
+        ROS_INFO("[Default] Frame Id is %s",frame_id.c_str());
+    }
+
+
     std::vector<int> paramlist;
 
     //load digital output pins
@@ -96,14 +153,52 @@ int main(int argc, char **argv){
             }
         }
     }
+
+    //load digital input pins
+    if (nh.getParam("upboard_ros/DigitalIn",paramlist)){
+        for (int i=0; i<paramlist.size(); i++){
+            try{
+                if (!checkDOParam(paramlist[i])){
+                    pGpio* tmp = new pGpio(paramlist[i]);
+                    tmp->dir(mraa::DIR_IN);
+                    listgpio_input.push_back(tmp);
+                    
+                    ROS_INFO("Pin %d is configured as Digital Input",paramlist[i]);
+                }
+                else{
+                    ROS_WARN("Pin %d is already configured as Digital Output", paramlist[i]);
+                }
+            }
+            catch(std::exception& e){
+                ROS_ERROR("%s",e.what());
+            }
+        }
+    }
     
 
     ros::Subscriber gpiosub = nh.subscribe("/upboard/gpio/write", 10, gpioCallback);
+    ros::Publisher gpiopub = nh.advertise<upboard_ros::ListGpio>("/upboard/gpio/read",10);
+    ros::Rate rate(frequency);
 
-    ros::spin();
+    upboard_ros::ListGpio gpio_msg;
+    
+    while (ros::ok()){
+        gpio_msg.gpio.clear();
+        for (int i=0; i<listgpio_input.size(); i++){
+            upboard_ros::Gpio tmp_msg;
+            tmp_msg.pin=listgpio_input[i]->getPin();
+            tmp_msg.value=getGpio(tmp_msg.pin);
+            gpio_msg.gpio.push_back(tmp_msg);
+        }
+        gpio_msg.header.stamp=ros::Time::now();
+        gpio_msg.header.frame_id=frame_id;
+        gpiopub.publish(gpio_msg);
+        ros::spinOnce();
+        rate.sleep();
+    }
 
     
-    //put all outputs to 0
+    //set all outputs to 0
     for (int i=0; i<listgpio_output.size(); i++){
         try{
             listgpio_output[i]->write(0);
